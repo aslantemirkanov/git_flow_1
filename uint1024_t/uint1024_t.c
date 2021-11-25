44413afd04ec19aeed40272a47a8ae818c96e30d
Aslan <aat0007@mail.ru>
2021-11-25 23:11:02 +0300

    added multiplication

--- uint1024_t/uint1024_t.c
+++ uint1024_t/uint1024_t.c
@@ -1,6 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
+struct uint1024_t mult_op(struct uint1024_t x, struct uint1024_t y) {
+    struct uint1024_t ans;
+    int len_x, len_y;
+    int tmp_ans[128];
+    for (int i = 0; i < 128; i++) tmp_ans[i] = 0;
+    int i = 127;
+    while (x.digit[i] == 0) i--;
+    len_x = i + 1;
+    i = 127;
+    while (y.digit[i] == 0) i--;
+    len_y = i + 1;
+    int len_ans = len_x + len_y + 1;
+    for (int ix = 0; ix < len_x; ix++) {
+        for (int iy = 0; iy < len_y; iy++) {
+            tmp_ans[ix + iy] += x.digit[ix] * y.digit[iy];
+        }
+    }
+    for (int i = 0; i < len_ans; i++) {
+        tmp_ans[i + 1] += tmp_ans[i] / 256;
+        tmp_ans[i] = tmp_ans[i] % 256;
+    }
+    for (int i = 0; i < 128; i++) {
+        ans.digit[i] = tmp_ans[i];
+    }
+    return ans;
+}
+
+struct uint1024_t from_uint(unsigned int x) {
+    struct uint1024_t rez;
+    for (int i = 0; i < 128; i++) rez.digit[i] = 0;
+    int i = 0;
+    while (x > 0) {
+        rez.digit[i++] = x % 256;
+        x = x / 256;
+    }
+    return rez;
+}
+
 int main() {
     struct uint1024_t A, B;
     scanf_value(&A);
